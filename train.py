6f70226b094a9dca5ae7356d8b7a8aca340d7451
Maurizio Pio Vergara <s346643@studenti.polito.it>
2025-12-01 11:11:30 +0100

    Add training and validation logic in train.py
    Implement training and validation functions for model.

---
+++ train.py
@@ -0,0 +1,76 @@
+# train epoch
+def train(epoch, model, train_loader, criterion, optimizer):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.cuda(), targets.cuda()
+
+        # definizione di outputs e perdite
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    train_loss = running_loss / len(train_loader)
+    train_accuracy = 100. * correct / total
+    print(f'Train Epoch: {epoch} Loss: {train_loss:.6f} Acc: {train_accuracy:.2f}%')
+
+# validate epoch
+def validate(model, val_loader, criterion):
+    model.eval()
+    val_loss = 0
+
+    correct, total = 0, 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(val_loader):
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+            # definizione di outputs e perdite
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # Nel val e test loop non c'è optimization (backpropagation)
+            #direttamente valutiamo perdite e accuratezza
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    val_loss = val_loss / len(val_loader)
+    val_accuracy = 100. * correct / total
+
+    print(f'Validation Loss: {val_loss:.6f} Acc: {val_accuracy:.2f}%')
+    return val_accuracy
+
+# training
+model = CustomNet().cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9) # overfitting -> ridurre lr da 0.001 a 0.0001
+
+best_acc = 0
+
+# Run the training process for {num_epochs} epochs
+num_epochs = 10
+for epoch in range(1, num_epochs + 1):
+    train(epoch, model, train_loader, criterion, optimizer)
+
+    # At the end of each training iteration, perform a validation step
+    val_accuracy = validate(model, val_loader, criterion)
+
+    # Best validation accuracy
+    best_acc = max(best_acc, val_accuracy)
+
+
+print(f'Best validation accuracy: {best_acc:.2f}%')
